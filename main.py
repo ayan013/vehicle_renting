e5b94ff2c8757f74e3ce0ad669e1c0b3ef2be101
ayan <ayan787@gmail.com>
2025-06-10 00:50:58 +0530

    Added code for Cars and bikes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from models.vehicle import Vehicle
 from models.car import Car
 from models.bike import Bike
+from models.client import Client
 from services.RentLogic import RentLogic
-
+from services.invoice import Invoice
+from models.rent_details import Rent_Details
 
 rent_logic=RentLogic(0.20,1500)
 
@@ -20,16 +22,53 @@ while True:
     print("----Menu----")
     print("1. Rent a Car")
     print("2. Rent a Bike")
-    choice = input(print("Enter your choice 1 or 2 : "))
+    print("3. Exit")
+    choice = input("Enter your choice 1 or 2 or 3: ")
     try:
-        if choice == 1:
+        if choice == "1":
             cars=rent_logic.show_cars()
             for car in cars:
                 print(f"Id: {car.id} - {car.brand} - {car.rent} per day")
-            car_choice = input(print("Tell me the ID of the car you want to rent:"))
-            days=input(print("For how many days:"))
+            car_choice = input("Tell me the ID of the car you want to rent:")
+            days=int(input("For how many days: "))
+            selected_car = None
+            for car in cars:
+                if car.id == car_choice:
+                    selected_car = car
+                    break
 
-    except:
+            name = input("Enter your Name: ")
+            email = input("Enter your email")
+            contact = int(input("Enter your mobile number: "))
+            client = Client(name,email,contact)
+            rental = Rent_Details(client=client,vehicle=selected_car,car=selected_car,bike=None,rent_logic=rent_logic)
+            invoice=Invoice(rental,days)
+            print(invoice.generate_invoice())
+        elif choice == "2":
+            bikes = rent_logic.show_bikes()
+            for bike in bikes:
+                print(f"Id: {bike.id} - {bike.brand} - {bike.rent} per day")
+            bike_choice = input("Tell me the ID of the bike you want to rent:")
+            days = int(input("For how many days:"))
+            selected_bike=None
+            for bike in bikes:
+                if bike.id == bike_choice:
+                    selected_bike = bike
+                    break
+
+            name = input("Enter your Name: ")
+            email = input("Enter your email")
+            contact = int(input("Enter your mobile number: "))
+            client = Client(name, email, contact)
+            rental = Rent_Details(client=client, vehicle=selected_bike, car=None , bike=selected_bike,
+                                  rent_logic=rent_logic)
+            invoice = Invoice(rental, days)
+            print(invoice.generate_invoice())
+        else:
+            print("Exiting")
+            break
+
+    except ValueError as e:
         print("Wrong Choice")
 
 
