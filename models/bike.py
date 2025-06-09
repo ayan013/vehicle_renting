fbbaf54cc59fe6a5a3c2ac00e6911b97e33fc12d
ayan <ayan787@gmail.com>
2025-06-09 13:42:16 +0530

    Basic structure for Bike class

---
+++ models/bike.py
@@ -0,0 +1,11 @@
+from models.vehicle import Vehicle
+
+class Bike(Vehicle):
+    def __init__(self, id: int, type: str, rent: int, is_available: bool, brand:str, cc:int, mileage:int):
+        super().__init__(id, type, rent, is_available)
+        self.brand = brand
+        self.cc = cc
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"Brand : {self.brand}\n CC : {self.cc}\n Mileage(Aprrox): {self.mileage}"
