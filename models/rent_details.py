32c04b7f4be2dd3d9bdfb067fd4667073c7e5fd5
ayan <ayan787@gmail.com>
2025-06-10 02:09:33 +0530

    Added condition to identify Car/Bike

--- models/rent_details.py
+++ models/rent_details.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from models.client import Client
 from models.vehicle import Vehicle
 from models.car import Car
@@ -8,8 +8,17 @@ from services.RentLogic import RentLogic
 @dataclass
 class Rent_Details:
     client:Client
-    vehicle:Vehicle
-    car:Car
-    bike:Bike
-    rent_logic:RentLogic
+    vehicle:Vehicle = field(default=None, init=False)
+    car:Car = None
+    bike:Bike = None
+    days: int = 1
+    rent_logic:RentLogic = None
+
+    def __post_init__(self):
+        if self.car:
+            self.vehicle = self.car
+        elif self.bike:
+            self.vehicle = self.bike
+        else:
+            raise ValueError("Either car or bike must be provided to Rent_Details.")
 
