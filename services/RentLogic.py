3d7d1ea776f254de6b41368c98201389e88c4558
ayan <ayan787@gmail.com>
2025-06-10 17:19:40 +0530

    separated inventory and rent logic

--- services/RentLogic.py
+++ services/RentLogic.py
@@ -2,23 +2,8 @@ from models.vehicle import Vehicle
 
 class RentLogic:
     def __init__(self,tax:float,discount:int):
-        self.cars=[]
-        self.bikes=[]
         self.tax = tax
         self.discount = discount
-
-
-    def add_car(self,car):
-        return self.cars.append(car)
-
-    def add_bike(self,bike):
-        return self.bikes.append(bike)
-
-    def show_cars(self):
-        return self.cars
-
-    def show_bikes(self):
-        return self.bikes
 
     def total_cost(self,vehicle: Vehicle,days: int) -> float:
         subtotal = vehicle.calculate_cost(days)
