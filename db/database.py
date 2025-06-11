0f4f1a1106c09b719134fd6945b68c774a763605
ayan <ayan787@gmail.com>
2025-06-11 18:04:02 +0530

    Added description for the file

--- db/database.py
+++ db/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# This file is responsible for creating database, session and declaring Base class
 DATABASE_URL="sqlite:///./rental.db"
 
 engine= create_engine(DATABASE_URL,connect_args={"check same thread": False})
