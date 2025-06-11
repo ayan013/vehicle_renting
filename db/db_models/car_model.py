0ffc3e5b025dd521586384ba94834a8fcd08da88
ayan <ayan787@gmail.com>
2025-06-11 16:51:01 +0530

    Created db model for Cars

---
+++ db/db_models/car_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, String, Boolean, Column
+from db.database import Base
+
+class CarModel(Base):
+    __tablename__ = "cars"
+
+    id = Column(Integer,primary_key=True,index=True)
+    type = Column(String,default="car")
+    rent = Column(Integer)
+    is_available = Column(Boolean,default=True)
+    brand = Column(String)
+    fuel_type = Column(String)
+    seater = Column(Integer,default=4)
